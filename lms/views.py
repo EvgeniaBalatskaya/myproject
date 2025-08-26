d6f0d63f45b14c5ce13d7f1cfae6603663428420
EvgeniaBalatskaya <evgeniabalackaa6@gmail.com>
2025-08-26 15:31:48 +0300

    Initial commit: Django project with users and lms apps

---
+++ lms/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics, viewsets
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+# Generic Views для уроков
+class LessonListCreateView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
