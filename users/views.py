782550acf52eaca9e54149a75c2177b18325a8fe
EvgeniaBalatskaya <evgeniabalackaa6@gmail.com>
2025-08-26 16:25:19 +0300

    Добавлены модель Payment, фикстура, CustomUser и сериализаторы

--- users/views.py
+++ users/views.py
@@ -1,7 +1,20 @@
 from rest_framework import viewsets
 from .models import CustomUser
 from .serializers import UserSerializer
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from .models import Payment
+from .serializers import PaymentSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['course', 'lesson', 'method']  # фильтры
+    ordering_fields = ['date']  # сортировка по дате
+    ordering = ['-date']  # сортировка по умолчанию
